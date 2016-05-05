7ea18729a932b391bd69ef9579c9edcc5c7eb104
michalp334 <michal.porwisz@gmail.com>
2016-05-05 03:12:02 +0200

    Update EditDistanceCalculator.java

--- src/EditDistanceCalculator.java
+++ src/EditDistanceCalculator.java
@@ -6,7 +6,23 @@ public class EditDistanceCalculator {
 	private static final int substitutionCostIfMatch = 0;
 	private static final int substitutionCostIfNoMatch = 2;
 	
-	public static int [][] calculateTable (String baseString, String targetString) {//transformation = insertion or deletion or substitution
+	public static void printEditDistance (String baseString, String targetString) { //print only final value
+		
+		editDistanceTable  = calculateTable(baseString, targetString);
+		
+	}
+	
+	public static void printEditDistanceTable (String baseString, String targetString) { //print whole table
+		
+		editDistanceTable  = calculateTable(baseString, targetString);
+			
+		for(int[] row : editDistanceTable) {
+		        FormattedTablePrint.printRow(row);
+	    	}
+
+	}
+	
+	private static int[][] calculateTable (String baseString, String targetString) {//transformation = insertion or deletion or substitution
 	
 		
 		int baseStringLength = baseString.length();
@@ -18,38 +34,37 @@ public class EditDistanceCalculator {
 		String[] baseStringArray = baseString.split("");
 		String[] targetStringArray = targetString.split("");
 		
-		
-			int costOfSubstitution; 
-	
-			
-			/*start with 1 as 0th row and column is already filled by the createBlankTable */
+			/*start with 1 as 0th row and column is already filled by the createBlankTable 
+			*/
 			for (int i = 1; i<=baseStringLength; i++){  
 				
 					for (int j = 1; j<=targetStringLength; j++) {		
 					
-						int insertion = editDistanceTable[i-1][j] + insertionCost;  
-						int deletion = editDistanceTable[i][j-1] + deletionCost;
-							
-						//-1 as array indexes start with 0 and our counter starts with 1
-							if (baseStringArray[i-1].equals(targetStringArray[j-1])) {
-								costOfSubstitution = substitutionCostIfMatch;
-							}
-				
-							else {
-								costOfSubstitution = substitutionCostIfNoMatch;
-							}
-			
-			
-						int substitution = editDistanceTable[i-1][j-1] + costOfSubstitution;
-						
-							editDistanceTable[i][j] = min(insertion, deletion, substitution);
-			
+						calculateEditDistance (i,j);
 					}	
-			}	
+			}
+		int editDistanceFinalValue
 		return editDistanceTable;
 	}
 	
-	
+	private static void calculateEditDistance(i, j) { //calculate i-th row and j-th column entry of editDistanceTable
+		int insertion = editDistanceTable[i-1][j] + insertionCost;  
+		int deletion = editDistanceTable[i][j-1] + deletionCost;
+							
+			//-1 as array indexes start with 0 and our counter starts with 1
+			if (baseStringArray[i-1].equals(targetStringArray[j-1])) {
+			costOfSubstitution = substitutionCostIfMatch;
+			}
+				
+			else {
+			costOfSubstitution = substitutionCostIfNoMatch;
+			}
+			
+			
+		int substitution = editDistanceTable[i-1][j-1] + costOfSubstitution;
+						
+		editDistanceTable[i][j] = min(insertion, deletion, substitution);
+	}
 	
 	private static int min(int a, int b, int c) {
 	    return Math.min(Math.min(a, b), c);
@@ -57,7 +72,7 @@ public class EditDistanceCalculator {
 	
 	
 	
-	private static int[][]  createBlankTable (int baseStringLength, int targetStringLength ) {
+	private static int[][]  createBlankTable (int baseStringLength, int targetStringLength ) { //initializa edit distance table
 
 		int [][] editDistanceTable;
 		// +1, as the 0th row and column is for the initialization values from blank table
@@ -82,10 +97,7 @@ public class EditDistanceCalculator {
 			
 			int[][] editDistanceTable = EditDistanceCalculator.calculateTable ("execution", "intention");
 			
-			 for(int[] row : editDistanceTable) {
-		        FormattedTablePrint.printRow(row);
-		    }
-
+		
 			
 		}
 
