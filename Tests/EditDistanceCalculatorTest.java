1a86cc7d77c204e892d52bf0f7d3d90788ede478
michalp334 <michal.porwisz@gmail.com>
2016-06-29 03:15:55 +0200

    Update EditDistanceCalculatorTest.java

--- Tests/EditDistanceCalculatorTest.java
+++ Tests/EditDistanceCalculatorTest.java
@@ -12,18 +12,18 @@ public class EditDistanceCalculatorTest {
 		EditDistanceTable table = EditDistanceCalculator.calculateTable("lollipop", "lollipop");
 		int[][] testTable = table.getFullTable();
 		
-		assertEquals("final edit distance between identical strings should to be 0", 0, testTable[8][8] );
+		assertEquals("final edit distance between identical strings should to be 0", 0, testTable.getFinalValue() );
 		
 		table = EditDistanceCalculator.calculateTable("execution", "intention");
 		testTable = table.getFullTable();
 		
-		assertEquals("final edit distance between intention and execution should to be 8", 8, testTable[9][9] ); 
+		assertEquals("final edit distance between intention and execution should to be 8", 8, testTable.getFinalValue() ); 
 		assertEquals("transformation cost between intent and execut should to be 8", 8, testTable[6][6] ); //test middle entries of table
 		
 		table = EditDistanceCalculator.calculateTable("mark", "a");
 		testTable = table.getFullTable();
 		
-		assertEquals("final edit distance between mark and a should to be 3", 3, testTable[4][1] );
+		assertEquals("final edit distance between mark and a should to be 3", 3, testTable.getFinalValue() );
 	}
 }
 
