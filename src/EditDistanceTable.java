e9b4e7d8c0cfa8af56e9e4c0611c80e79f5324c7
michalp334 <michal.porwisz@gmail.com>
2016-06-29 03:14:01 +0200

    Update EditDistanceTable.java

--- src/EditDistanceTable.java
+++ src/EditDistanceTable.java
@@ -2,16 +2,9 @@
 public class EditDistanceTable {
 	
 	private int[][] fullTable;
-	private int finalValue;
-	
 	
 	public void setFullTable(int[][] table) {
 		this.fullTable = table;
-		int maxRowIndex = table.length-1; //-1 because of 0-th index
-		int maxColumnIndex = table[1].length-1; //-1 because of 0-th index	
-		
-		//auto-update finalValue as it's strictly connected with the whole table
-		this.finalValue = table[maxRowIndex][maxColumnIndex]; 
 	}
 	
 	
@@ -20,7 +13,9 @@ public class EditDistanceTable {
 	}
 	
 	public int getFinalValue() {
-		return this.finalValue;
+		int maxRowIndex = this.fullTable.length-1; //-1 because of 0-th index
+		int maxColumnIndex = this.fullTable[1].length-1; //-1 because of 0-th index	
+		return this.fullTable[maxRowIndex][maxColumnIndex];
 	}
 	
 	//print whole table
